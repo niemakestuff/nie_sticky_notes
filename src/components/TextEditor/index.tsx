65ae447dda0927472a21b3ba04738459ef7b69f5
Nie <niemakestuff@proton.me>
2026-07-10 03:08:07 +0000

    Fix text editor padding

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -10,7 +10,14 @@ import { HEX_TEXT_LIGHT } from "../../constants";
 import useWindowFocus from "../../hooks/useWindowFocus";
 import BottomButtons from "./BottomButtons";
 
-export function initUseEditor(content: string, options?: UseEditorOptions) {
+export function initUseEditor(
+    content: string,
+    options?: UseEditorOptions,
+
+    // Card previews (TextEditorReadOnly) rely on this exact default. Do NOT change.
+    // I repeat, DO NOT change!!
+    typography: string = "font-['Segoe_UI',sans-serif] text-[14px] leading-[19px]",
+) {
     return useEditor({
         extensions: [
             StarterKit,
@@ -24,12 +31,9 @@ export function initUseEditor(content: string, options?: UseEditorOptions) {
         editorProps: {
             attributes: {
                 class: [
-                    "min-h-full",
-                    "pl-3 pr-1 pt-0 pb-2",
-                    "font-['Segoe_UI',sans-serif] text-[14px] leading-[17.5px]",
-                    "outline-none caret-current",
+                    typography,
+                    "outline-none caret-current min-h-full",
                     "[&_p]:m-0",
-                    "[&_p+p]:mt-0.5",
                     "[&_ul]:list-disc [&_ul]:pl-5 [&_ul]:m-0",
                     "[&_ol]:list-decimal [&_ol]:pl-5 [&_ol]:m-0",
                     "[&_li+li]:mt-px",
@@ -47,46 +51,55 @@ export function initUseEditor(content: string, options?: UseEditorOptions) {
 
 export default function TextEditor({ note }: { note: Note }) {
     const isFocused = useWindowFocus();
-    const editor = initUseEditor(note.content, {
-        onUpdate: async ({ editor }) => {
-            note.content = editor.getHTML();
-            note.modifiedAt = new Date();
+    const editor = initUseEditor(
+        note.content,
+        {
+            onUpdate: async ({ editor }) => {
+                note.content = editor.getHTML();
+                note.modifiedAt = new Date();
 
-            const res = await ResultAsync.fromThrowable(invoke)("update_note", {
-                note: note,
-            });
+                const res = await ResultAsync.fromThrowable(invoke)(
+                    "update_note",
+                    {
+                        note: note,
+                    },
+                );
 
-            if (res.isErr()) alert(res.error);
+                if (res.isErr()) alert(res.error);
+            },
         },
-    });
+        "font-['Segoe_UI',sans-serif] text-[14px] leading-[18.4px]",
+    );
 
     return (
         <div
             className="flex flex-col flex-1 min-h-0"
             style={{ color: HEX_TEXT_LIGHT }}
         >
-            <EditorContent
-                editor={editor}
-                style={{ "--selection-color": note.color } as CSSProperties}
-                className={[
-                    "text-[#eeeeee]",
-                    "flex-1 min-h-0 overflow-y-auto",
-                    "selection:bg-[var(--selection-color)]",
-                    "[&_*::selection]:bg-[var(--selection-color)]",
-                    "[&::-webkit-scrollbar]:w-2.5",
-                    "[&::-webkit-scrollbar-track]:bg-transparent",
-                    "[&::-webkit-scrollbar-thumb]:bg-white/40",
-                    "[&::-webkit-scrollbar-thumb]:rounded-full",
-                    "[&::-webkit-scrollbar-thumb]:border-4",
-                    "[&::-webkit-scrollbar-thumb]:border-transparent",
-                    "[&::-webkit-scrollbar-thumb]:bg-clip-padding",
-                    "[&::-webkit-scrollbar-thumb:hover]:bg-white/70",
-                    !isFocused &&
-                        "[&_p.is-editor-empty:first-child::before]:!content-none",
-                ]
-                    .filter(Boolean)
-                    .join(" ")}
-            />
+            <div className="flex flex-col flex-1 min-h-0 pt-[2px] pb-[8px]">
+                <EditorContent
+                    editor={editor}
+                    style={{ "--selection-color": note.color } as CSSProperties}
+                    className={[
+                        "text-[#eeeeee] pl-3 pr-[4px]",
+                        "flex-1 min-h-0 overflow-y-auto [scrollbar-gutter:stable]",
+                        "selection:bg-[var(--selection-color)]",
+                        "[&_*::selection]:bg-[var(--selection-color)]",
+                        "[&::-webkit-scrollbar]:w-2.5",
+                        "[&::-webkit-scrollbar-track]:bg-transparent",
+                        "[&::-webkit-scrollbar-thumb]:bg-white/40",
+                        "[&::-webkit-scrollbar-thumb]:rounded-full",
+                        "[&::-webkit-scrollbar-thumb]:border-4",
+                        "[&::-webkit-scrollbar-thumb]:border-transparent",
+                        "[&::-webkit-scrollbar-thumb]:bg-clip-padding",
+                        "[&::-webkit-scrollbar-thumb:hover]:bg-white/70",
+                        !isFocused &&
+                            "[&_p.is-editor-empty:first-child::before]:!content-none",
+                    ]
+                        .filter(Boolean)
+                        .join(" ")}
+                />
+            </div>
 
             {isFocused && editor && <BottomButtons editor={editor} />}
         </div>
