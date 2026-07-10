4eba7d87c98a9a55eb40b25d69c5ac2134805786
Nie <niemakestuff@proton.me>
2026-07-10 17:53:47 +0000

    Refactor NoteWindow

---
+++ src/components/NoteWindow/TopBar.tsx
@@ -0,0 +1,121 @@
+import { Dispatch, SetStateAction } from "react";
+import { Dialog } from "@base-ui/react/dialog";
+import { invoke } from "@tauri-apps/api/core";
+import { getCurrentWindow } from "@tauri-apps/api/window";
+import { ResultAsync } from "neverthrow";
+import {
+    AddRegular,
+    DismissRegular,
+    MoreHorizontalRegular,
+} from "@fluentui/react-icons";
+import { Note } from "../../types";
+import useWindowFocus from "../../hooks/useWindowFocus";
+import Hover from "../../components/Hover";
+import { HEX_TEXT_LIGHT, HEX_TEXT_DARK } from "../../constants";
+import { isNoteEmpty } from "../../utils";
+import DropdownPanel from "./DropdownPanel";
+
+export default function TopBar({
+    note,
+    setNote,
+}: {
+    note: Note | null;
+    setNote: Dispatch<SetStateAction<Note | null>>;
+}) {
+    const isFocused = useWindowFocus();
+
+    return (
+        <div className="h-10 bg-transparent">
+            <div
+                data-tauri-drag-region
+                style={{
+                    color:
+                        note?.isColorDark === false
+                            ? HEX_TEXT_DARK
+                            : HEX_TEXT_LIGHT,
+                    backgroundColor: note?.color || "#000000",
+                }}
+                className={
+                    "flex justify-between transition-[height] duration-100 ease-out p-0" +
+                    " " +
+                    (isFocused ? "h-8" : "h-2")
+                }
+            >
+                {isFocused && (
+                    <>
+                        <Hover>
+                            <button
+                                className="w-8 h-full flex items-center justify-center"
+                                onClick={async () => {
+                                    const res =
+                                        await ResultAsync.fromThrowable(invoke)(
+                                            "create_note",
+                                        );
+
+                                    if (res.isErr()) alert(res.error);
+                                }}
+                            >
+                                <AddRegular fontSize={20} />
+                            </button>
+                        </Hover>
+
+                        <div className="flex">
+                            <Dialog.Root>
+                                <Hover>
+                                    <Dialog.Trigger className="w-8 h-full flex items-center justify-center outline-none">
+                                        <MoreHorizontalRegular fontSize={20} />
+                                    </Dialog.Trigger>
+                                </Hover>
+
+                                <Dialog.Portal>
+                                    <Dialog.Backdrop
+                                        className={[
+                                            "fixed inset-0 z-40 bg-mid-dark/60",
+                                            "transition-opacity duration-150 ease-out",
+                                            "data-[ending-style]:opacity-0 data-[starting-style]:opacity-0",
+                                        ].join(" ")}
+                                    />
+
+                                    <Dialog.Popup
+                                        className={[
+                                            "fixed inset-x-0 top-0 z-50 outline-none",
+                                            "transition-transform duration-150 ease-out",
+                                            "data-[ending-style]:-translate-y-full data-[starting-style]:-translate-y-full",
+                                        ].join(" ")}
+                                    >
+                                        <DropdownPanel
+                                            note={note}
+                                            setNote={setNote}
+                                        />
+                                    </Dialog.Popup>
+                                </Dialog.Portal>
+                            </Dialog.Root>
+
+                            <Hover>
+                                <button
+                                    className="w-8 h-full flex items-center justify-center"
+                                    onClick={async () => {
+                                        if (note && isNoteEmpty(note)) {
+                                            const res =
+                                                await ResultAsync.fromThrowable(
+                                                    invoke,
+                                                )("delete_note", {
+                                                    noteId: note.id,
+                                                });
+
+                                            if (res.isErr()) alert(res.error);
+                                        }
+
+                                        getCurrentWindow().close();
+                                    }}
+                                >
+                                    <DismissRegular fontSize={20} />
+                                </button>
+                            </Hover>
+                        </div>
+                    </>
+                )}
+            </div>
+        </div>
+    );
+}
--- src/components/NoteWindow/index.tsx
+++ src/components/NoteWindow/index.tsx
@@ -1,28 +1,13 @@
 import { useState, useEffect } from "react";
-import { Dialog } from "@base-ui/react/dialog";
 import { invoke } from "@tauri-apps/api/core";
-import { getCurrentWindow } from "@tauri-apps/api/window";
 import { ResultAsync } from "neverthrow";
-import {
-    AddRegular,
-    DismissRegular,
-    MoreHorizontalRegular,
-} from "@fluentui/react-icons";
 import { Note, RawNote } from "../../types";
-import useWindowFocus from "../../hooks/useWindowFocus";
-import Hover from "../../components/Hover";
 import TextEditor from "../../components/TextEditor";
-import { HEX_TEXT_LIGHT, HEX_TEXT_DARK } from "../../constants";
-import { unrawNote, isNoteEmpty } from "../../utils";
-import DropdownPanel from "./DropdownPanel";
+import { unrawNote } from "../../utils";
+import TopBar from "./TopBar";
 
 export default function NoteWindow({ noteId }: { noteId: string }) {
     const [note, setNote] = useState<Note | null>(null);
-    const isFocused = useWindowFocus();
-    const titleBarClassName =
-        "flex justify-between transition-[height] duration-100 ease-out p-0" +
-        " " +
-        (isFocused ? "h-8" : "h-2");
 
     useEffect(() => {
         ResultAsync.fromThrowable(invoke)<RawNote>("get_note", {
@@ -37,97 +22,7 @@ export default function NoteWindow({ noteId }: { noteId: string }) {
 
     return (
         <div className="bg-mid-dark h-full flex flex-col">
-            <div className="h-10 bg-transparent">
-                <div
-                    data-tauri-drag-region
-                    style={{
-                        color:
-                            note?.isColorDark === false
-                                ? HEX_TEXT_DARK
-                                : HEX_TEXT_LIGHT,
-                        backgroundColor: note?.color || "#000000",
-                    }}
-                    className={titleBarClassName}
-                >
-                    {isFocused && (
-                        <>
-                            <Hover>
-                                <button
-                                    className="w-8 h-full flex items-center justify-center"
-                                    onClick={async () => {
-                                        const res =
-                                            await ResultAsync.fromThrowable(
-                                                invoke,
-                                            )("create_note");
-
-                                        if (res.isErr()) alert(res.error);
-                                    }}
-                                >
-                                    <AddRegular fontSize={20} />
-                                </button>
-                            </Hover>
-
-                            <div className="flex">
-                                <Dialog.Root>
-                                    <Hover>
-                                        <Dialog.Trigger className="w-8 h-full flex items-center justify-center outline-none">
-                                            <MoreHorizontalRegular
-                                                fontSize={20}
-                                            />
-                                        </Dialog.Trigger>
-                                    </Hover>
-
-                                    <Dialog.Portal>
-                                        <Dialog.Backdrop
-                                            className={[
-                                                "fixed inset-0 z-40 bg-mid-dark/60",
-                                                "transition-opacity duration-150 ease-out",
-                                                "data-[ending-style]:opacity-0 data-[starting-style]:opacity-0",
-                                            ].join(" ")}
-                                        />
-
-                                        <Dialog.Popup
-                                            className={[
-                                                "fixed inset-x-0 top-0 z-50 outline-none",
-                                                "transition-transform duration-150 ease-out",
-                                                "data-[ending-style]:-translate-y-full data-[starting-style]:-translate-y-full",
-                                            ].join(" ")}
-                                        >
-                                            <DropdownPanel
-                                                note={note}
-                                                setNote={setNote}
-                                            />
-                                        </Dialog.Popup>
-                                    </Dialog.Portal>
-                                </Dialog.Root>
-
-                                <Hover>
-                                    <button
-                                        className="w-8 h-full flex items-center justify-center"
-                                        onClick={async () => {
-                                            if (note && isNoteEmpty(note)) {
-                                                const res =
-                                                    await ResultAsync.fromThrowable(
-                                                        invoke,
-                                                    )("delete_note", {
-                                                        noteId: note.id,
-                                                    });
-
-                                                if (res.isErr())
-                                                    alert(res.error);
-                                            }
-
-                                            getCurrentWindow().close();
-                                        }}
-                                    >
-                                        <DismissRegular fontSize={20} />
-                                    </button>
-                                </Hover>
-                            </div>
-                        </>
-                    )}
-                </div>
-            </div>
+            <TopBar note={note} setNote={setNote} />
 
             {note !== null && <TextEditor note={note} />}
         </div>
